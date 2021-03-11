6d5189f30cac5dc6be2675019870ae2c43427868
wangs258 <877395874@qq.com>
2021-03-11 17:52:02 +0800

    第五次提交，修改端口

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("test3 %s!", name);
+		return String.format("aaa %s!", name);
 	}
 
 }
